2335e75a6eaabac7b4147ceae5a197a727c4ab24
Rutash Joshipura <joshipurarutash@gmail.com>
2020-07-30 12:20:07 +0530

    Make code more readable

--- src/App.js
+++ src/App.js
@@ -1,45 +1,47 @@
-import React, { Component } from 'react';
-import Table from './Table';
-import tableData from './tableData7Records.json';
+import React, { Component } from "react";
+import Table from "./Table";
+import tableData from "./tableData7Records.json";
 
 class App extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            tableData
-            
-        };
-
-    }
-
-    handleChange = (value, index, id) => {    
-        const { tableData } = this.state;
-        const newTableData = [
-            ...tableData.slice(0, index),{id:id, value:value},
-            ...tableData.slice(index+1)
-          ];
-        this.setState({tableData: newTableData});  
-    }
-
-
-    removeData = (index) => {
-        const { tableData } = this.state;
-        const newTableData = [
-            ...tableData.slice(0, index),
-            ...tableData.slice(index+1)
-          ];
-        this.setState({tableData: newTableData});
+  constructor(props) {
+    super(props);
+    this.state = {
+      tableData,
     };
+  }
 
-    render() {
-        const { tableData } = this.state;
+  handleChange = (value, index, id) => {
+    const { tableData } = this.state;
+    const newTableData = [
+      ...tableData.slice(0, index),
+      { id: id, value: value },
+      ...tableData.slice(index + 1),
+    ];
+    this.setState({ tableData: newTableData });
+  };
 
-        return (
-            <div className="container">
-                <Table tableData={tableData} removeData={this.removeData} handleChange={this.handleChange} />
-            </div>
-        )
-    };
+  removeData = (index) => {
+    const { tableData } = this.state;
+    const newTableData = [
+      ...tableData.slice(0, index),
+      ...tableData.slice(index + 1),
+    ];
+    this.setState({ tableData: newTableData });
+  };
+
+  render() {
+    const { tableData } = this.state;
+
+    return (
+      <div className="container">
+        <Table
+          tableData={tableData}
+          removeData={this.removeData}
+          handleChange={this.handleChange}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/Table.js
+++ src/Table.js
@@ -1,62 +1,70 @@
-import React, { Component, useState, useEffect } from 'react'
+import React, { Component, useState, useEffect } from "react";
 
 const TableHeader = () => {
-    return (
-        <thead>
-            <tr>
-                <th>ID</th>
-                <th>Value</th>
-            </tr>
-        </thead>
-    )
+  return (
+    <thead>
+      <tr>
+        <th>ID</th>
+        <th>Value</th>
+      </tr>
+    </thead>
+  );
 };
 
 const TableBody = (props) => {
+  const [buttonText, setButtonText] = useState("Edit");
 
-    const [buttonText, setButtonText] = useState("Edit");
+  const [thing, setThing] = useState(props.thing.value);
+  const [disabled, setDisabled] = useState(true);
+  function editText() {
+    setDisabled(!disabled);
+    if (disabled) setButtonText("Save");
+    else {
+      props.handleChange(thing, props.index, props.thing.id);
 
-    const [thing, setThing] = useState(props.thing.value);
-    const [disabled, setDisabled] = useState(true);
-    function editText() {
-        setDisabled(!disabled);
-        if(disabled)
-        setButtonText("Save");
-        else {
-            props.handleChange(thing, props.index, props.thing.id)
-          
-            setButtonText("Edit")
-
-        }
-        
-        
+      setButtonText("Edit");
     }
-    useEffect(() => {setThing(props.thing.value)}, [props.thing])
-    const rows =  (
-            <tr>
-                <td>{props.thing.id}</td>
-        <td><input type="text" value={thing} onChange={e =>setThing(e.target.value)} disabled={disabled}></input></td>
-                <td>
-                    &nbsp;<button onClick={editText}>{buttonText}</button>&nbsp;&nbsp;
-                    <button onClick={() => props.removeData(props.index)}>Delete</button>
-                </td>
-            </tr>
-        )
-    
+  }
+  useEffect(() => {
+    setThing(props.thing.value);
+  }, [props.thing]);
+  const rows = (
+    <tr>
+      <td>{props.thing.id}</td>
+      <td>
+        <input
+          type="text"
+          value={thing}
+          onChange={(e) => setThing(e.target.value)}
+          disabled={disabled}
+        ></input>
+      </td>
+      <td>
+        &nbsp;<button onClick={editText}>{buttonText}</button>&nbsp;&nbsp;
+        <button onClick={() => props.removeData(props.index)}>Delete</button>
+      </td>
+    </tr>
+  );
 
-    return <tbody>{rows}</tbody>
+  return <tbody>{rows}</tbody>;
 };
 
 const Table = (props) => {
-    const { tableData, removeData, handleChange } = props
-    return (
-      
-        <table>
-            <TableHeader />
-    {props.tableData.map((row, index) => <TableBody index={index} key={index} thing={row} removeData={removeData} handleChange={handleChange} /> )}
-        
-            
-        </table>
-    )
-}
+  const { tableData, removeData, handleChange } = props;
+  return (
+    <table>
+      <TableHeader />
+      {props.tableData.map((row, index) => (
+        <TableBody
+          index={index}
+          key={index}
+          thing={row}
+          removeData={removeData}
+          handleChange={handleChange}
+        />
+      ))}
+    </table>
+  );
+};
 
 export default Table;
